1250d862440f167c0bc87d432ca5037f7c036cd4
GuJauk <wkdnr1254@gmail.com>
2024-09-20 16:11:19 +0900

    기본적인 전체 틀 완성

--- demo/src/main/java/com/example/demo/Order.java
+++ demo/src/main/java/com/example/demo/Order.java
@@ -5,9 +5,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
+@Table(name = "purchases") // 테이블 이름을 purchases로 수정
 @Data
 public class Order {
     @Id
