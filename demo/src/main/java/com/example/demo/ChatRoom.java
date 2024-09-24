da50068f08cc2cd97593e029d04e4d4023eab54a
GuJauk <wkdnr1254@gmail.com>
2024-09-24 14:15:16 +0900

    채팅방 백엔드 클래스들 생성ㅇ

--- demo/src/main/java/com/example/demo/ChatRoom.java
+++ demo/src/main/java/com/example/demo/ChatRoom.java
@@ -15,9 +15,9 @@ public class ChatRoom {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // 채팅방 ID
 
-    @ManyToOne // 고객과 다대일 관계 설정
+    @ManyToOne // 사용자와 다대일 관계 설정
     @JoinColumn(name = "user_id") // 외래 키 설정
-    private User user; // 고객 정보
+    private User user; // 고객 정보, 필드 이름을 user로 변경
 
     // 관리자는 하나로 고정, 필드로 설정할 필요 없음
     private Long adminId; // 관리자의 ID
